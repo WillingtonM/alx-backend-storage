4b02d8ce10fe55bfe48b3fa775a4d8514e0b2020
WillingtonM <willington.mhlanga@gmail.com>
2024-01-24 22:50:26 +0200

    0x02. Redis basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -17,18 +17,22 @@ def count_url_response(method: Callable):
     """
 
     @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        s_redis.incr(f'count:{url}')
-        result = s_redis.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        s_redis.set(f'count:{url}', 0)
-        s_redis.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
+    def wrapper(url):
+        """wrapper function"""
+        key = "cached:" + url
+        cached_value = s_redis.get(key)
+        if cached_value:
+            return cached_value.decode("utf-8")
+
+            # Get new content and update cache
+        key_count = "count:" + url
+        html_content = method(url)
+
+        s_redis.incr(key_count)
+        s_redis.set(key, html_content, ex=10)
+        s_redis.expire(key, 10)
+        return html_content
+    return wrapper
 
 
 @count_url_response
@@ -37,3 +41,7 @@ def get_page(url: str) -> str:
         Return HTML content
     """
     return requests.get(url).text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
