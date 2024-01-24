a0bbb619a85a61e71f5fc82be336eeabb386c837
WillingtonM <willington.mhlanga@gmail.com>
2024-01-24 22:19:14 +0200

    0x02. Redis basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -17,17 +17,18 @@ def count_url_response(method: Callable):
     """
 
     @wraps(method)
-    def wrapper(url):  # sourcery skip: use-named-expression
-        """ Wrapper for decorator """
-        s_redis.incr(f"count:{url}")
-        cached_html = s_redis.get(f"cached:{url}")
-        if cached_html:
-            return cached_html.decode('utf-8')
-        html = method(url)
-        s_redis.setex(f"cached:{url}", 10, html)
-        return html
-
-    return wrapper
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        s_redis.incr(f'count:{url}')
+        result = s_redis.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        s_redis.set(f'count:{url}', 0)
+        s_redis.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
 
 
 @count_url_response
@@ -35,5 +36,4 @@ def get_page(url: str) -> str:
     """
         Return HTML content
     """
-    resp = requests.get(url)
-    return resp.text
+    return requests.get(url).text
