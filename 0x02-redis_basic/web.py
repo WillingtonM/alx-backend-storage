9156e709d4b3cf4e5e3199dbbb799c3f2fd96218
WillingtonM <willington.mhlanga@gmail.com>
2024-01-24 23:42:03 +0200

    0x02. Redis basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,20 +2,53 @@
 """
     Module for web cache and tracker
 """
+import typing
+import requests_html
+from functools import wraps
 import redis
 import requests
-s_redis = redis.Redis()
-count = 0
 
 
-def get_page(url: str) -> str:
-    """ get a page and cach value"""
-    s_redis.set(f"cached:{url}", count)
-    resp = requests.get(url)
-    s_redis.incr(f"count:{url}")
-    s_redis.setex(f"cached:{url}", 10, s_redis.get(f"cached:{url}"))
-    return resp.text
+def counting(method: typing.Callable):
+    """
+    Counts  calls to input method
+    """
+
+    @wraps(method)
+    def count(self, url: str, *args, **kwargs) -> typing.Callable:
+        """
+        Caches counts of visits to url
+        """
+        key = 'count:' + url
+        self._redis.incr(key)
+        self._redis.expire(key, 10)
+        return counting(url, *args, **kwargs)
+    return count
 
 
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+class Cache:
+    """
+    This class queries a webpage and monitors number of visits to it
+    """
+
+    def __init__(self):
+        """
+        Initialise class
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    @counting
+    def get_page(self, url: str) -> str:
+        """
+        This function ses the requests module to obtain
+        """
+        response = requests.get(url)
+        return response.text
+
+
+if __name__ == '__main__':
+    url = 'http://slowwly.robertomurray.co.uk'
+    cache = Cache()
+    print(cache)
+    cache.get_page(url)
