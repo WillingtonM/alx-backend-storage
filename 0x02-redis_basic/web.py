8c15d05fb9f4d7950bf8e10cfa472a8f7f132685
WillingtonM <willington.mhlanga@gmail.com>
2024-01-24 23:33:08 +0200

    0x02. Redis basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,45 +2,20 @@
 """
 Module for web cache and tracker
 """
-from datetime import timedelta
-from typing import Callable
-import requests
-from functools import wraps
+
 import redis
-
+import requests
 s_redis = redis.Redis()
+count = 0
 
 
-def count_url_response(method: Callable):
-    """
-        Decorator counting how many times URL is accessed
-    """
-
-    @wraps(method)
-    def wrapper(url):
-        """wrapper function"""
-        key = "cached:" + url
-        cached_value = s_redis.get(key)
-        if cached_value:
-            return cached_value.decode("utf-8")
-
-            # Get new content and update cache
-        key_count = "count:" + url
-        html_content = method(url)
-
-        s_redis.incr(key_count)
-        s_redis.set(key, html_content, ex=10)
-        s_redis.expire(key, 10)
-        return html_content
-    return wrapper
-
-
-@count_url_response
 def get_page(url: str) -> str:
-    """
-        Return HTML content
-    """
-    return requests.get(url).text
+    """ get a page and cach value"""
+    s_redis.set(f"cached:{url}", count)
+    resp = requests.get(url)
+    s_redis.incr(f"count:{url}")
+    s_redis.setex(f"cached:{url}", 10, s_redis.get(f"cached:{url}"))
+    return resp.text
 
 
 if __name__ == "__main__":
